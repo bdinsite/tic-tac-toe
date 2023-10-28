70b4f90f304202e58856ae96f51b82689fe398cf
bdinsite <bdinsight.oy@gmail.com>
2023-10-28 12:57:20 +0300

    Add game logic and check for winner

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,7 +34,6 @@ function newGame() {
 }
 
 function startGame() {
-    //const selectedCell = document.querySelectorAll('.boardCol div');
     const selectedCell = document.querySelectorAll('.boardCol button');
 
     selectedCell.forEach(cell => cell.addEventListener('click', clickedCell));
@@ -43,11 +42,13 @@ function startGame() {
         evt.target.textContent = 'X';
         evt.target.classList = 'unavailable';
         evt.target.disabled = true;
-        computerMove();
+        //computerMove();
+        checkWinner('Player');
     }
     
 }
 
+// COMPUTER
 function computerMove() {
     console.log('Computer is thinking...');
     //const selectedCell = document.querySelectorAll('.boardCol div');
@@ -62,18 +63,127 @@ function computerMove() {
     });
 
     // Pick random empty cell
-    if(availableCells.length > 0) {
+    if(!emptyCells(availableCells, 'Computer')){
+        pickRandomCell(availableCells, 'Computer');
+    };
+}
+
+function emptyCells(availableCells, player) {
+    if(availableCells.length == 0) {
+        checkWinner(player);
+        return true;
+    }
+}
+
+function pickRandomCell(availableCells, player) {
     const pickItem = Math.floor(Math.random()*availableCells.length);
     const arrItem = availableCells[pickItem];
     const cellTarget = document.getElementById(arrItem);
     cellTarget.textContent = 'O';
-    cellTarget.classList = 'unavailable';
     cellTarget.disabled = true;
-    } else {
-        gameOver();
+    checkWinner(player);
+}
+
+
+// CHECK WINNER
+function checkWinner(player) {
+    const playerArr = [];
+    const computerArr = [];
+    const cellContent = document.querySelectorAll('.boardCol button');
+    cellContent.forEach(cell => {
+        if(cell.textContent == 'X'){
+            playerArr.push(cell.id);
+        } else if(cell.textContent == 'O'){
+            computerArr.push(cell.id);
+        }
+    });
+    checkArray(playerArr, computerArr, player);
+    //console.log(playerArr);    
+}
+
+function checkArray(playerArr, computerArr, player) {
+    if(playerArr.includes("00") && 
+        ((playerArr.includes("01") && playerArr.includes("02")) ||
+        (playerArr.includes("10") && playerArr.includes("20")) ||
+        (playerArr.includes("11") && playerArr.includes("22")))) {
+            gameOver('Player wins!');
+            return;
+        }
+    else if(playerArr.includes("10") &&
+        playerArr.includes("11") &&
+        playerArr.includes("12")) {
+            gameOver('Player wins!');
+            return;
+        }
+    else if(playerArr.includes("20") &&
+        playerArr.includes("21") &&
+        playerArr.includes("22")) {
+            gameOver('Player wins!');
+            return;
+        }
+    else if(playerArr.includes("01") &&
+        playerArr.includes("11") &&
+        playerArr.includes("21")) {
+            gameOver('Player wins!');
+            return;
+        }   
+    else if(playerArr.includes("02") && 
+        ((playerArr.includes("12") && playerArr.includes("22")) ||
+        (playerArr.includes("11") && playerArr.includes("20")))) {
+            gameOver('Player wins!');
+            return;
+        }
+
+    else if(computerArr.includes("00") && 
+        ((computerArr.includes("01") && computerArr.includes("02")) ||
+        (computerArr.includes("10") && computerArr.includes("20")) ||
+        (computerArr.includes("11") && computerArr.includes("22")))) {
+            gameOver('Computer wins!');
+            return;
+        }
+    else if(computerArr.includes("10") &&
+        computerArr.includes("11") &&
+        computerArr.includes("12")) {
+            gameOver('Computer wins!');
+            return;
+        }
+    else if(computerArr.includes("20") &&
+        computerArr.includes("21") &&
+        computerArr.includes("22")) {
+            gameOver('Computer wins!');
+            return;
+        }
+    else if(computerArr.includes("01") &&
+        computerArr.includes("11") &&
+        computerArr.includes("21")) {
+            gameOver('Computer wins!');
+            return;
+        }
+    else if(computerArr.includes("02") && 
+        ((computerArr.includes("12") && computerArr.includes("22")) ||
+        (computerArr.includes("11") && computerArr.includes("20")))) {
+            gameOver('Computer wins!');
+            return;
+        }
+    ;
+    checkTie(playerArr, computerArr, player);
+}
+
+function checkTie(playerArr, computerArr, player) {
+    if(playerArr.length + computerArr.lengt == 12) {
+        gameOver("It's a bloody tie!");
+    }
+    checkTurn(player);
+}
+
+function checkTurn(player) {
+    if(player == 'Player'){
+        computerMove();
     }
 }
 
-function gameOver() {
-    console.log("That's all, folks!")
+function gameOver(input) {
+    const cells = document.querySelectorAll('.boardCol button');
+    cells.forEach(cell => cell.disabled = true);
+    console.log(input);
 }
